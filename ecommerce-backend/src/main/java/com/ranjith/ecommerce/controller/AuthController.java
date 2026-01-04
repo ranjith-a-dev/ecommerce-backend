5a81077f2ac8e60cd118af7a151cc80fb134f35c
RANJITH A <ranjithannadurai7@gmail.com>
2026-01-04 09:45:25 +0530

    removed unwanted import

--- ecommerce-backend/src/main/java/com/ranjith/ecommerce/controller/AuthController.java
+++ ecommerce-backend/src/main/java/com/ranjith/ecommerce/controller/AuthController.java
@@ -2,7 +2,7 @@ package com.ranjith.ecommerce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
