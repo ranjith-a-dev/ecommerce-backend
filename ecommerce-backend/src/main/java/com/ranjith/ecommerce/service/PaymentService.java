f2454fb0d6d03ba116fe8abc2b104d86fe7f162d
RANJITH A <ranjithannadurai7@gmail.com>
2026-01-06 19:09:43 +0530

    removed unused imports

--- ecommerce-backend/src/main/java/com/ranjith/ecommerce/service/PaymentService.java
+++ ecommerce-backend/src/main/java/com/ranjith/ecommerce/service/PaymentService.java
@@ -3,10 +3,8 @@ package com.ranjith.ecommerce.service;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.ranjith.ecommerce.dto.PaymentResponseDTO;
 import com.ranjith.ecommerce.entity.Order;
